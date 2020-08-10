c482df8a761b76172ba3bd5aebd862ba59007c92
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-10 12:05:42 +0530

    background color added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hi</h1>
     </div>
   );
 }
