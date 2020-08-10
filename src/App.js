318b9e810a0d96043a5d2a3c6ce24f2c9aa1bef5
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-08-10 15:11:23 +0530

    Added jumbotron

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 import "./App.css";
-import NavBar from './components/NavBar/NavBar';
-import Demo from './components/Demo/Demo';
+import NavBar from "./components/NavBar/NavBar";
+import Demo from "./components/Demo/Demo";
+import Jumbo from "./components/Jumbo/Jumbo";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Demo />
+      <Container>
+        <Demo />
+        <Jumbo />
+      </Container>
     </div>
   );
 }
